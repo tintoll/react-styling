c7df824ccc85429e64ee26c2bb3c9edb05913200
blue <blue@tigrison.com>
2018-05-10 11:12:38 +0900

    styled-component 맛보기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './App.scss';
 import classNames from 'classnames/bind';
 import Button from './components/Button';
+import StyledButton from './components/StyledButton'
 
 const cx = classNames.bind(styles);
 console.log(styles);
@@ -11,7 +12,7 @@ class App extends Component {
     const isBlue = false;
     return (
       <div>
-        <Button>버튼</Button>
+        <StyledButton big>버튼</StyledButton>
       </div>
 
     );
